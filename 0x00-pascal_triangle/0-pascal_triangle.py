9772457c18ba74e4f5338d1977374f9db092cc8f
Juanse1595 <jspbescos15@gmail.com>
2022-04-14 17:50:02 -0500

    FIX little error at line 21

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,7 +18,7 @@ def pascal_triangle(n):
     for i in range(n - 1):
         temp = [0] + triangle[-1] + [0]
         row = []
-        for j in range(len(triangle[-1] + 1)):
+        for j in range(len(triangle[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         triangle.append(row)
     return triangle
