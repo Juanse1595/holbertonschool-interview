3497c3670daa60fe3f2148d7ab824aea7ec972f9
Juanse1595 <3309@holberstonschool.com>
2022-04-22 17:41:20 -0500

    FIX style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,6 +14,7 @@ Write a method that determines if all the boxes can be opened.
 - Return True if all boxes can be opened, else return False
 '''
 
+
 def canUnlockAll(boxes):
     '''
     Function to determine if all boxes can be opened
@@ -61,4 +62,3 @@ def canUnlockAll(boxes):
 #                 '''If keySet has all the keys, return true'''
 #                 return True
 #     return False
-
