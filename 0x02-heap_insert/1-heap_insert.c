1b4d49f63a5889efad1b28155bb339bcdb12d494
Juanse1595 <3309@holberstonschool.com>
2022-05-08 19:25:55 -0500

    UPD add tree_length function

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * tree_lenght - calculates the lenght of a binary tree
+ * @root: pointer to the head of the tree
+ * 
+ * Return: length of the tree
+ */
+
+int tree_length(heap_t **root)
+{
+  if (root == NULL || *root == NULL)
+    return (0);
+  
+  return (tree_length(&((*root)->left)) + tree_length(&((*root)->right)) + 1);
+}
+
+
+
+/**
  * heap_insert - inserts a value into a Max Binary Heap
  * @root: double pointer to root
  * @value: value of the new node
