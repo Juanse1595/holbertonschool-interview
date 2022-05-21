b8d3f9377ec95fb2ff4dd84831303a2784883174
Juanse1595 <jspbescos15@gmail.com>
2022-05-21 11:53:12 -0500

    Fixed betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -22,6 +22,13 @@ static void print_grid(int grid[3][3])
 	}
 }
 
+/**
+ * sandpiles_sum - sim sandpiles until grid1 is stable
+ *
+ * @grid1: 3x3 grid
+ * @grid2: another 3x3 grid to be add with grid1
+ */
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int  i, j;
@@ -42,6 +49,13 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	}
 }
 
+
+/**
+ * check_stable_sandpile - checks if sandpiles are stable
+ * @grid1: 3x3 to be checked
+ * Return: 0 if stable, 1 otherwise
+ */
+
 int check_stable_sandpile(int grid1[3][3])
 {
 	int i, j;
@@ -57,6 +71,11 @@ int check_stable_sandpile(int grid1[3][3])
 	return (0);
 }
 
+
+/**
+ * topple_sandpile - topples the sandpile that it receives
+ * @grid1: sandpile to be toppled
+ */
 
 void topple_sandpile(int grid1[3][3])
 {
@@ -78,27 +97,17 @@ void topple_sandpile(int grid1[3][3])
 				if (grid1[i - 1][j] > 3)
 					result_grid[i][j] += 1;
 			if ((j - 1) >= 0 && (j - 1) < 3)
-			{
 				if (grid1[i][j - 1] > 3)
 					result_grid[i][j] += 1;
-			}
 			if ((i + 1) >= 0 && (i + 1) < 3)
-			{
 				if (grid1[i + 1][j] > 3)
 					result_grid[i][j] += 1;
-			}
 			if ((j + 1) >= 0 && (j + 1) < 3)
-			{
 				if (grid1[i][j + 1] > 3)
 					result_grid[i][j] += 1;
-			}
 		}
 	}
 	for (i = 0; i < 3; i++)
-	{
 		for (j = 0; j < 3; j++)
-		{
 			grid1[i][j] = result_grid[i][j];
-		}
-	}
 }
