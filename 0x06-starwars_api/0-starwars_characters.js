c02e46aafc6cbab13667a7c38dc6354fb8c963b5
Juanse1595 <jspbescos15@gmail.com>
2022-05-26 23:19:44 -0500

    FIX semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
 
-if (!process.argv[2])
-{
-    console.log('Usage: ./0-starwars_characters.js movie_id')
-    process.exit(1);
+if (!process.argv[2]) {
+  console.log('Usage: ./0-starwars_characters.js movieId');
+  process.exit(1);
 }
 
 let characters = false;
 request('https://swapi-api.hbtn.io/api/films', (error, response, body) => {
-    body = JSON.parse(body);
-    movie_id = process.argv[2];
-    results = body.results;
-    for (const movie of results) {
-        if (movie.episode_id == movie_id) {
-            characters = movie.characters;
-        }
+  error && console.error('Error: ', error);
+  body = JSON.parse(body);
+  const movieId = process.argv[2];
+  const results = body.results;
+  for (const movie of results) {
+    if (movie.episode_id === movieId) { characters = movie.characters; }
+  }
+  if (characters) {
+    for (const char of characters) {
+      request(char, (error, response, body) => {
+        error && console.error('Error: ', error);
+        console.log(JSON.parse(body).name);
+      });
     }
-    if (characters) {
-        for (const char of characters) {
-            request(char, (error, response, body) => {
-                console.log(JSON.parse(body).name);
-            })
-        }
-    }
-})
+  }
+});
