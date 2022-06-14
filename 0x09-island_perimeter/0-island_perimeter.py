f496f5a06ae9e8d1893dcd547488191ba4f47bbd
Juanse1595 <jspbescos15@gmail.com>
2022-06-13 21:25:05 -0500

    ADD 0th task file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,6 @@
 '''
-Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+Create a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid:
 
 - grid is a list of list of integers:
   - 0 represents water
@@ -9,5 +10,40 @@ Create a function def island_perimeter(grid): that returns the perimeter of the 
   - grid is rectangular, with its width and height not exceeding 100
 - The grid is completely surrounded by water
 - There is only one island (or nothing).
-- The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+- The island doesnt have “lakes” (water inside that isnt connected
+  to the water surrounding the island).
 '''
+
+
+def island_perimeter(grid):
+    '''
+    Calculater the perimeter of the grid
+    '''
+    perimeter = 0
+    h = len(grid)
+    w = len(grid[0])
+    for i in range(0, h):
+        for j in range(0, w):
+            'if current square is a 1 or land'
+            if grid[i][j] == 1:
+                'if upper row is in range'
+                if i - 1 >= 0:
+                    'if upper square is water'
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                'if lower roe is in range'
+                if i + 1 < h:
+                    'if lower square is water'
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                'if previous column is in range'
+                if j - 1 >= 0:
+                    'if previous square is water'
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                'if next column is in range'
+                if j + 1 < w:
+                    'if next square is water'
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+    return perimeter
